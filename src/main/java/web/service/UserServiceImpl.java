05068eed0d0a4d1c862166358e59e708cee029d0
kamil <akchurinkami@Gmail.com>
2023-06-11 14:43:21 +0300

    last

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -8,33 +8,38 @@ import web.entity.User;
 
 import java.util.List;
 @Service
-@Transactional
+
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDAO userDAO;
 
     @Override
+    @Transactional
     public List<User> getAllUser() {
         return userDAO.getAllUser();
     }
 
     @Override
+    @Transactional
     public void addUsers(User user) {
         userDAO.addUsers(user);
     }
 
     @Override
+    @Transactional
     public User userById(int id) {
         return userDAO.userById(id);
     }
 
     @Override
+    @Transactional
     public void update(int id, User user) {
         userDAO.update(id,user);
     }
 
     @Override
+    @Transactional
     public void delete(int id) {
         userDAO.delete(id);
     }
